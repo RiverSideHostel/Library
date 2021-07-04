6eac617038f56b7f17b10dd827775b322e4e5050
Miyakawa Takuya <omyakawa@gmail.com>
2021-07-05 02:32:41 +0900

    ベースからのちょい更新

---
+++ users_login.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/kasho.css">
+  <link rel="icon" href="image/icon.png">
+  <title>ログイン画面</title>
+</head>
+
+<body>
+
+  <!-- 大きい箱　幅をCssで指定 -->
+
+  <section class="form_parent">
+
+
+    <img src="./image/rogo.png">
+
+
+    <h1>Stillartにログイン</h1>
+
+
+    <!-- ログイン -->
+    <form class="form" action="./users_login_act.php" method="post">
+
+
+      <div>
+        <input type="text" class="login_username" name="user_name" placeholder="ユーザー名">
+      </div>
+      <div>
+        <input type="password" class="login_password" name="password" placeholder="パスワード">
+      </div>
+
+      <div>
+        <button class="action_btn">ログイン</button>
+      </div>
+
+    </form>
+
+    <!-- 新規会員登録 -->
+
+    <div class="person_box">
+      <p class="person_text">アカウントをお持ちでない方</p>
+      <button class="action_btn" onclick="location.href='./users/users_input.php'">新規会員登録</a>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="kasho.js"></script>
+</body>
+
+</html>
